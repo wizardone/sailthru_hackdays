73ae9d7d9eb3934e39ed6f21cacab8d5084a232d
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2021-06-23 10:00:41 +1200

    add user class

---
+++ user.py
@@ -0,0 +1,8 @@
+class User:
+    email = 'no@no.com'
+    def __init__(name, age):
+        self.name = name
+        self.age = age
+
+    def print:
+        print("Name: "+ self.name + "; Age: "+ self.age)
