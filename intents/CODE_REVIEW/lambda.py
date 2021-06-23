bcd4104acb43b34321666c582a2bba5adc56fd95
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2021-06-24 09:34:29 +1200

    add refined lambda

--- intents/CODE_REVIEW/lambda.py
+++ intents/CODE_REVIEW/lambda.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
     repository = event['currentIntent']['slotDetails']['repository']['resolutions'][0]['value']
     branch_name = event['currentIntent']['slotDetails']['branch']['resolutions'][0]['value']
     
-    response = client.create_code_review(
+    client.create_code_review(
         Name=repository + '-'+ branch_name + '-' + secrets.token_hex(16),
         RepositoryAssociationArn="arn:aws:codeguru-reviewer:us-east-1:892579079126:association:eb328a26-5f01-4cf6-89ee-11f7567ed397",
         Type={
@@ -18,9 +18,17 @@ def lambda_handler(event, context):
             }
         }
     )
-    print(response)
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Ok')
+    
+    ok_response = {
+        "dialogAction": {
+            "type": "Close",
+            "fulfillmentState": "Fulfilled",
+            "message": {
+                "contentType": "PlainText",
+                "content": "Thank you, we've started reviewing your code"
+            }
+        }
     }
+    
+    return ok_response
 
